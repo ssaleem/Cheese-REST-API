8ff1067450b917e15ef476c5f763646e71647ef9
ssaleem <sara@sarasaleem.com>
2019-05-01 14:47:16 -0400

    Add main application class

---
+++ src/main/java/com/cheeseapi/cheeseapi/CheeseapiApplication.java
@@ -0,0 +1,13 @@
+package com.cheeseapi.cheeseapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheeseapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheeseapiApplication.class, args);
+	}
+
+}
