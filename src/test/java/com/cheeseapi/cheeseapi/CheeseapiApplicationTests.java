6a1c4c3811ecb07cb798d41347b7f01716415061
ssaleem <sara@sarasaleem.com>
2019-05-01 14:50:44 -0400

    Add main test class

---
+++ src/test/java/com/cheeseapi/cheeseapi/CheeseapiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cheeseapi.cheeseapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CheeseapiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
